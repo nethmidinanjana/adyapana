d84fb244808e4b3a95d7b03706b3e2e74f2f0709
nethmidinanjana <nethmidinanjana02@gmail.com>
2025-03-30 10:25:01 +0530

    Adding project

---
+++ src/model/Details.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class Details {
+    
+    private String subject;
+    private String teacher;
+    private String timeslot;
+    private String payment;
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the teacher
+     */
+    public String getTeacher() {
+        return teacher;
+    }
+
+    /**
+     * @param teacher the teacher to set
+     */
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    /**
+     * @return the timeslot
+     */
+    public String getTimeslot() {
+        return timeslot;
+    }
+
+    /**
+     * @param timeslot the timeslot to set
+     */
+    public void setTimeslot(String timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    /**
+     * @return the payment
+     */
+    public String getPayment() {
+        return payment;
+    }
+
+    /**
+     * @param payment the payment to set
+     */
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+    
+}
